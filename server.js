0e66ad766b10da096e6d281a2963944b92553127
AnthonyTurijan <anthonyturijan@gmail.com>
2022-03-21 20:07:23 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,21 @@
+//DEPENDENCIES
+const express = require('express')
+const { append } = require('vary')
+
+//CONFIG
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+//ROUTES
+app.get('/', (req,res) =>{
+    res.send('Welcome to an Awesome App about Breads!')
+})
+
+//BREADS
+const breadsController = require('./controllers/bread_controllers.js')
+app.use('/breads', breadsController)
+//LISTEN
+app.listen(PORT, () =>{
+    console.log('Server On')
+})
