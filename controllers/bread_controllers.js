8cb43183f3cc4f68ef0660fb6b87918f6e809fe9
AnthonyTurijan <anthonyturijan@gmail.com>
2022-04-03 11:15:47 -0700

    Added MondoDB corrections

--- controllers/bread_controllers.js
+++ controllers/bread_controllers.js
@@ -3,18 +3,33 @@ const breads = express.Router()
 const Bread = require('../models/bread.js')
 
 //INDEX
-breads.get('/', (req,res) =>{
-    res.render('Index',
-    {
-        breads: Bread,
-        title: 'Index Page'
-    })
-   // res.send(Bread)
+breads.get('/', (req, res) => {
+  Bread.find()
+      .then(foundBreads => {
+          console.log(foundBreads),
+          res.render('index',{
+            breads: foundBreads,
+            title: 'Index Page'
+          })
+      })
 })
 
 //NEW
 breads.get('/new', (req,res)=>{
   res.status(200).render('new')
+})
+
+//GET
+breads.get('/:id', (req,res)=>{
+  Bread.findById(req.params.id)
+    .then(foundBread => {
+      res.render('show', {
+        bread: foundBread
+      })
+    })
+    .catch(err => {
+      res.send('404')
+    })
 })
 
 //EDIT
@@ -46,14 +61,14 @@ breads.delete('/:indexArray', (req,res)=>{
 // CREATE
 breads.post('/', (req, res) => {
   if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    req.body.image = undefined
   }
   if(req.body.hasGluten === 'on') {
     req.body.hasGluten = 'true'
   } else {
     req.body.hasGluten = 'false'
   }
-  Bread.push(req.body)
+  Bread.create(req.body)
   res.redirect('/breads')
 })
 
